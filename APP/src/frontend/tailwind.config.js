4c97f7c781c76880721918c8424482cc3cb86e14
yuto1127 <yuto.aka1127@gmail.com>
2025-05-15 12:08:50 +0900

    環境構築

---
+++ APP/src/frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./src/**/*.{js,ts,jsx,tsx}",  // JSX/TSX のパスに合わせて調整
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
