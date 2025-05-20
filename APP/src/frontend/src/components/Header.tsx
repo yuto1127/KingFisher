a2514f139cad60776b72b7f9250b2c449c6aa986
ryuki0616 <f23ba035@chuo.ac.jp>
2025-05-20 11:10:11 +0900

    ヘッダーの色変更

--- APP/src/frontend/src/components/Header.tsx
+++ APP/src/frontend/src/components/Header.tsx
@@ -31,7 +31,7 @@ const Header: React.FC<HeaderProps> = ({ role }) => {
   };
 
   return (
-    <AppBar position="static">
+    <AppBar position="static" sx={{ backgroundColor: '#009a73' }}>
       <Toolbar>
         <Typography variant="h6" component="div" sx={{ flexGrow: 0, mr: 4 }}>
           KingFisher
