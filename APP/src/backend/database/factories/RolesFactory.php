d6545a729f97ba995805700baa922f3f31e29866
yuto1127 <yuto.aka1127@gmail.com>
2025-05-22 13:44:01 +0900

    Roleテーブルの初期値設定用処理作成

---
+++ APP/src/backend/database/factories/RolesFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class RolesFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        static $id = 1; // 連番の初期値を設定
+
+        $names = ['管理者', '一般', '顧客']; // nameの候補
+
+        return [
+            'id' => $id++, // 連番でidを設定
+            'name' => $this->faker->unique()->randomElement($names), // nameをランダムに選択
+        ];
+    }
+}
