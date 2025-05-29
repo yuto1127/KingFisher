5cb9c429ad41b8c8e75673bbec38c6e0695f1245
yuto1127 <yuto.aka1127@gmail.com>
2025-05-29 10:13:16 +0900

    命名規則準拠

--- APP/src/backend/database/migrations/2025_05_26_000640_create_helpdesks_table.php
+++ APP/src/backend/database/migrations/2025_05_26_000640_create_helpdesks_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('helpdesks', function (Blueprint $table) {
+        Schema::create('help_desks', function (Blueprint $table) {
             $table->id();
             $table->foreignId('role_id')->constrained('roles');
             $table->foreignId('user_id')->constrained('users');
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('helpdesks');
+        Schema::dropIfExists('help_desks');
     }
 };
