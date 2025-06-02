d906b1be5fd13752687dfe60d39afd5d613f5dae
yuto1127 <yuto.aka1127@gmail.com>
2025-06-02 09:58:20 +0900

    リネーム

--- APP/src/backend/database/migrations/2025_05_29_045415_create_user_passes_table.php
+++ APP/src/backend/database/migrations/2025_05_29_045415_create_user_passes_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('userpass', function (Blueprint $table) {
+        Schema::create('user_pass', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('users');
             $table->string('email');
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('userpass');
+        Schema::dropIfExists('user_pass');
     }
 };
