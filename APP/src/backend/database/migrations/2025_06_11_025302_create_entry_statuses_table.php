bbe07cd07e900faaefb8d462b6ba207655e675d6
yuto1127 <yuto.aka1127@gmail.com>
2025-06-16 09:54:05 +0900

    マイグレーション変更

--- APP/src/backend/database/migrations/2025_06_11_025302_create_entry_statuses_table.php
+++ APP/src/backend/database/migrations/2025_06_11_025302_create_entry_statuses_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger('event_id');
             $table->unsignedBigInteger('user_id');
+            $table->string('status');
             $table->timestamps();
 
             // 外部キー制約
