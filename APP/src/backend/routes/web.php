6da3b619f02c75f25a6b1cbc96abd9e8881ab541
yuto1127 <yuto.aka1127@gmail.com>
2025-06-17 09:49:09 +0900

    変更

--- APP/src/backend/routes/web.php
+++ APP/src/backend/routes/web.php
@@ -3,10 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 // routes/web.php
-Route::get('/login', function () {
-    // ログイン画面のビューを返す
-    return view('login');
-})->name('login');
+// Route::get('/login', function () {
+//     // ログイン画面のビューを返す
+//     return view('login');
+// })->name('login');
 
 Route::get('/', function () {
     return view('welcome');
