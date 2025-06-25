72279003389187497b11280dc7ad2aa60b35d98a
ryuki0616 <f23ba035@chuo.ac.jp>
2025-06-25 14:21:33 +0900

    path ch

--- APP/src/frontend/lib/services/auth_api.dart
+++ APP/src/frontend/lib/services/auth_api.dart
@@ -9,7 +9,8 @@ import '../models/login_model.dart';
 import 'package:flutter/foundation.dart';
 
 class AuthApi {
-  static const String baseUrl = 'http://54.165.66.148/api';
+  // static const String baseUrl = 'http://54.165.66.148/api';
+  static const String baseUrl = 'https://cid-kingfisher.jp/api';
   static const String tokenKey = 'auth_token';
   static const String userKey = 'user_data';
 
