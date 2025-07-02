63224150b0bfe960ff856788d651c61143e5ccff
yuto1127 <yuto.aka1127@gmail.com>
2025-07-02 05:34:04 +0000

    DB追加

--- APP/src/backend/database/seeders/PointsSeeder.php
+++ APP/src/backend/database/seeders/PointsSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Point;
 
 class PointsSeeder extends Seeder
 {
