74ee3c7f867f18be6cbac46d24d7a21babceb066
yuto1127 <yuto.aka1127@gmail.com>
2025-07-03 11:38:16 +0900

    パス変更

--- APP/src/frontend/lib/utils/network_utils.dart
+++ APP/src/frontend/lib/utils/network_utils.dart
@@ -7,7 +7,7 @@ class NetworkUtils {
     try {
       final response = await http
           .get(
-            Uri.parse('$baseUrl/ping'),
+            Uri.parse('$baseUrl/api/ping'),
           )
           .timeout(const Duration(seconds: 3));
       return response.statusCode == 200;
